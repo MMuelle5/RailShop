2998c9efde5519ba84d0fcefcf6c8cc4b34de466
Marius Mueller <mmuelle5@hsz-t.ch>
2012-03-06 10:18:16 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailShop::Application.config.secret_token = 'cecf1a9ba2961e7c1438a90a587ef4781ceb2cbd0818d2172d953b12c5329ba7b83f2b02426ae0cda8231d2bc2844de9442f0b664201d4df4e6ac09fffc4d091'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailShop::Application.config.session_store :cookie_store, key: '_RailShop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailShop::Application.config.session_store :active_record_store
